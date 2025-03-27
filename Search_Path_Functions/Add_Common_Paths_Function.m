28b78e26f6f98570d6a879c94c05b3d6dd29c8fe
Andrew Dries <andrew.dries@corp.advancednavigation.com>
2025-03-27 09:53:54 -0400

    Update Add_Common_Paths_Function.m
    Update common paths to reflect new GIT path

--- Search_Path_Functions/Add_Common_Paths_Function.m
+++ Search_Path_Functions/Add_Common_Paths_Function.m
@@ -2,8 +2,8 @@ function Add_Common_Paths_Function()
 %This function is used to add common MATLAB paths to the MATLAB search path
 
     %Set folder paths
-    FolderPath{1} = 'C:\Users\andrewdries\Documents\MATLAB';
-    FolderPath{2} = 'C:\Users\andrewdries\Documents\NAS\Sandbox\octave';
+    FolderPath{1} = 'C:\Users\andrewdries\Documents\MATLAB\Support\ANPP-MATLAB-Support\';
+    %FolderPath{2} = 'C:\Users\andrewdries\Documents\NAS\Sandbox\octave';
     
     %Call add paths recursive
     for i = 1:length(FolderPath)
